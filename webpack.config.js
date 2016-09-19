5f90ae9f50ee3b75c7da2e0aa2478ee00638224b
Martin Patino <mpatino117@gmail.com>
2016-09-18 19:48:36 -0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+let extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
+
+
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/client/index.html',
   filename: 'index.html',
@@ -18,8 +22,11 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
+    }, {
+      test: /\.scss$/i,
+      loader: extractCSS.extract(['css', 'sass'])
     }]
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [HtmlWebpackPluginConfig, extractCSS], 
   devtool: '#source-map'
-};
+}
